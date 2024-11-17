490c850dfd72e3e5cfdc3b19120232ce5ba94a1f
CHAERYEON-LEE <sogogijjang@naver.com>
2024-11-17 19:08:31 +0900

    프론트 프로젝트 생성

---
+++ front-project/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          리액트 프로젝트 띄우기 성공!!!!!!!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hahaha😀
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
